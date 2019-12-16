0d3abbef24d67843fb76abd133cc1ca6fe673f24
Adam West <jsarry@gmail.com>
2019-12-16 19:41:45 +0200

    Use sunnys lib

--- Edge Sensors/src/main.cpp
+++ Edge Sensors/src/main.cpp
@@ -1,5 +1,6 @@
 #include <ConfigManager.h>
 #include <esp_now.h>
+#include <WifiEspNow.h>
 #include <WiFi.h>
 
 // Global copy of slave
@@ -14,17 +15,14 @@ void deletePeer();
 bool manageSlave();
 void ScanForSlave();
 void InitESPNow();
+
+
 // Init ESP Now with fallback
 void InitESPNow() {
   WiFi.disconnect();
-  if (esp_now_init() == ESP_OK) {
-    Serial.println("ESPNow Init Success");
-  }
-  else {
-    Serial.println("ESPNow Init Failed");
-    // Retry InitESPNow, add a counte and then restart?
-    // InitESPNow();
-    // or Simply Restart
+  bool ok = WifiEspNow.begin();
+  if (!ok) {
+    Serial.println("WifiEspNow.begin() failed");
     ESP.restart();
   }
 }
@@ -108,31 +106,11 @@ bool manageSlave() {
       return true;
     } else {
       // Slave not paired, attempt pair
-      esp_err_t addStatus = esp_now_add_peer(&slave);
-      if (addStatus == ESP_OK) {
-        // Pair success
-        Serial.println("Pair success");
-        return true;
-      } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
-        // How did we get so far!!
-        Serial.println("ESPNOW Not Init");
-        return false;
-      } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
-        Serial.println("Invalid Argument");
-        return false;
-      } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
-        Serial.println("Peer list full");
-        return false;
-      } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
-        Serial.println("Out of memory");
-        return false;
-      } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
-        Serial.println("Peer Exists");
-        return true;
-      } else {
-        Serial.println("Not sure what happened");
-        return false;
-      }
+      bool ok;
+      ok = WifiEspNow.addPeer(slave.peer_addr);
+        if (!ok) {
+            Serial.println("WifiEspNow.addPeer() failed");
+        }
     }
   } else {
     // No slave found to process
@@ -159,29 +137,21 @@ void deletePeer() {
   }
 }
 
-char hello[] = "hello world";
 
 void sendData() {
-  const uint8_t *peer_addr = slave.peer_addr;
-  Serial.print("Sending: "); Serial.println(hello);
-  esp_err_t result = esp_now_send(NULL, (uint8_t*) hello, sizeof(hello));
-  Serial.print("Send Status: ");
-  if (result == ESP_OK) {
-    Serial.println("Success");
-  } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
-    // How did we get so far!!
-    Serial.println("ESPNOW not Init.");
-  } else if (result == ESP_ERR_ESPNOW_ARG) {
-    Serial.println("Invalid Argument");
-  } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
-    Serial.println("Internal Error");
-  } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
-    Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-  } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
-    Serial.println("Peer not found.");
-  } else {
-    Serial.println("Not sure what happened");
-  }
+    const uint8_t *peer_addr = slave.peer_addr;
+    char msg[60];
+    int len = snprintf(msg, sizeof(msg), "hello ESP-NOW from %s at %lu", WiFi.softAPmacAddress().c_str(), millis());
+    WifiEspNow.send(slave.peer_addr, reinterpret_cast<const uint8_t*>(msg), len);
+    Serial.print("Sending: "); Serial.println(msg);
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 struct Config {
@@ -223,16 +193,17 @@ void setup() {
     configManager.setAPICallback(createCustomRoute);
 
     configManager.begin(config);
-  //Set device in STA mode to begin with
-  WiFi.mode(WIFI_STA);
-  Serial.println("ESPNow/Basic/Master Example");
-  // This is the mac address of the Master in Station Mode
-  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
-  // Init ESPNow with a fallback logic
-  InitESPNow();
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
-  esp_now_register_send_cb(OnDataSent);
+    //Set device in STA mode to begin with
+    WiFi.mode(WIFI_STA);
+    Serial.println("ESPNow/Basic/Master Example");
+    // This is the mac address of the Master in Station Mode
+    Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+    // Init ESPNow with a fallback logic
+    
+    InitESPNow();
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(OnDataSent);
 }
 
 void loop() {
